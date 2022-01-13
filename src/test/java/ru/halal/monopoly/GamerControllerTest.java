7a04de148bb853357db812c0311d62b3797944c8
AliAlievALive <madarat@mail.ru>
2022-01-13 18:04:11 +0300

    Tests is works

--- src/test/java/ru/halal/monopoly/GamerControllerTest.java
+++ src/test/java/ru/halal/monopoly/GamerControllerTest.java
@@ -9,12 +9,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.halal.monopoly.controller.GamerController;
 import ru.halal.monopoly.domain.Gamer;
 
 import java.util.ArrayList;
 
 import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -23,27 +23,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestPropertySource("/application-test.properties")
-@Sql(value = {"/gamers-list-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-@Sql(value = {"/gamers-list-after.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class GamerControllerTest {
-    @Autowired
-    private GamerController controller;
     @Autowired
     private MockMvc mockMvc;
 
-//    @Test
-//    void gamersGetsTest() throws Exception {
-//        this.mockMvc.perform(get("/api/gamer/list"))
-//                .andDo(print())
-//                .andExpect(status().isOk());
-//                .andExpect(content().string(containsString("Gamers list limited 10")));
-//    }
+    @Test
+    @Sql(value = {"/gamers-list-after.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    void gamersGetsTest() throws Exception {
+        this.mockMvc.perform(get("/api/gamer/list"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Gamers list limited 10")));
+    }
+
 
     @Test
+    @Sql(value = {"/gamers-list-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     void saveGamerTest() throws Exception {
         this.mockMvc.perform(post("/api/gamer/save")
                         .content(asJsonString(
-                                new Gamer(0, "ALi", 15000,
+                                new Gamer(0, "Антон", 15000,
                                         new ArrayList<>(), new ArrayList<>(), new ArrayList<>())))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
--- src/test/resources/gamers-list-after.sql
+++ src/test/resources/gamers-list-after.sql
@@ -1,4 +1,6 @@
 drop table airport;
 drop table city;
 drop table communal;
-drop table gamer
+drop table gamer;
+drop table flyway_schema_history;
+drop table hibernate_sequence;
