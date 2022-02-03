82006361be58cf59b41be4a463874b4fe2bf9788
AliAlievALive <madarat@mail.ru>
2022-02-03 09:03:18 +0300

    change Communal behaviour

--- src/main/java/ru/halal/monopoly/domain/ownerships/Communal.java
+++ src/main/java/ru/halal/monopoly/domain/ownerships/Communal.java
@@ -1,14 +1,23 @@
 package ru.halal.monopoly.domain.ownerships;
 
 import lombok.*;
+import ru.halal.monopoly.domain.ownerships.interfaces.CommunalCostCalc;
 
 import javax.persistence.Entity;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Getter
 @Setter
 @ToString
-public class Communal extends Ownership {
+public class Communal extends Ownership implements CommunalCostCalc {
+    private int multiplier = 4;
+    @Override
+    public int increasePay() {
+        return multiplier = 10;
+    }
+
+    @Override
+    public int decreasePay() {
+        return multiplier = 4;
+    }
 }
